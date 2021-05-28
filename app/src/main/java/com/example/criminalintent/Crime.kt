03e77a3f983ea12c7f50f15530ea4c1515e82dd4
Fernando Barra <fbarra@garnetinstruments.com>
2021-05-28 12:26:35 -0600

    Fixes typo

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -4,5 +4,5 @@ import java.util.*
 
 data class Crime(   val id: UUID = UUID.randomUUID(),
                     var title: String = "",
-                    var data: Date = Date(),
+                    var date: Date = Date(),
                     var isSolved: Boolean = false)
